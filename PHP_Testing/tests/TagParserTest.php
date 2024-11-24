8ab41ce26da42bf9fbe6417b5e797cdd212b4b1c
tevimmanuel71 <tevimmanuel71@gmail.com>
2024-11-24 14:50:46 +0000

    Laravel Testing-Laracast

---
+++ PHP_Testing/tests/TagParserTest.php
@@ -0,0 +1,57 @@
+<?php
+  
+namespace Tests;
+
+use App\TagParser;
+use PHPUnit\Framework\TestCase;
+
+class TagParserTest extends TestCase
+{
+	protected TagParser $parser;
+
+	protected function setUp():void
+	{
+		$this->parser = new TagParser();
+	}
+
+  private function crot()
+  {
+    $data = Data::all();
+  }
+  
+  // personal, money, family
+  public function test_it_a_single_tag()
+  {
+    $result = $this->parser->parse('personal');
+    $expected = ['personal']; 
+
+    $this->assertSame($expected, $result);
+  }
+  public function test_it_parses_a_comma_separated_list_of_tag()
+  {
+    $result = $this->parser->parse('personal, money, family');
+    $expected = ['personal', 'money', 'family'];
+
+    $this->assertSame($expected, $result);
+  }
+  public function test_it_parses_a_pipe_separated_list_of_tags()
+  {
+    $result = $this->parser->parse('personal | money | family');
+    $expected = ['personal', 'money', 'family'];
+
+    $this->assertSame($expected, $result);
+  }
+  public function test_spaces_are_optional()
+  {
+    $result = $this->parser->parse('personal,money,family');
+    $expected = ['personal', 'money', 'family'];
+
+    $this->assertSame($expected, $result);
+
+    $result = $this->parser->parse('personal|money|family');
+    $expected = ['personal', 'money', 'family'];
+
+    $this->assertSame($expected, $result);
+  }
+
+}
---
+++ default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.php81
+    pkgs.php81Packages.composer
+  ];
+}
